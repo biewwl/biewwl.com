9d05fb8cb6075fcafa69a2b5cb4449a43ac4ea1d
Gabriel Dias <gabriel.dias.fern@gmail.com>
2023-08-07 15:26:14 -0300

    FIX: Remove console.log

--- src/redux/reducer/theme.js
+++ src/redux/reducer/theme.js
@@ -3,7 +3,6 @@ import lS from "manager-local-storage";
 const preferDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
 const autoTheme = preferDark ? "dark" : "light";
-console.log(preferDark);
 const initialState = lS.get("biewwl.com_theme") ?? { theme: autoTheme };
 
 export const CHANGE_THEME = "CHANGE_THEME";
